3071e044ef2019d172e9b4797371cc1f3cbe3a6a
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-03-09 19:51:17 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/testing', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message: "Hello World" })
+      });
+
+      if (!response.ok) {
+        console.log("Error");
+      } else {
+        console.log("Success");
+      }
+
+      const data = await response.json();
+      console.log("Response:", data);
+
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  }
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button onClick={handleSubmit}>Click Here</button>
+    </>
+  )
+}
+
+export default App;
